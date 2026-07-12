f743d0805ed07352fd8bf28ac2f5547ce70dc604
didolawm-hash <didolawm@gmail.com>
2026-07-12 05:06:20 +0300

    Refactor KomaruPatch memory functions
    Updated memory management functions to improve safety and performance.

--- TheosMMTempKomaru/utils/KPatch.hpp
+++ TheosMMTempKomaru/utils/KPatch.hpp
@@ -1,11 +1,3 @@
-/*
-IOS Theos Template Komaru
-Jailed (NoJB) Mod Menu Template for iOS Games
-By aq9
-https://github.com/VenerableCode/iOS-Theos-ModMenuTemp-NoJB
-*/
-
-
 #pragma once
 
 #include <iostream>
@@ -15,38 +7,31 @@ https://github.com/VenerableCode/iOS-Theos-ModMenuTemp-NoJB
 
 class KomaruPatch {
 public:
+    // Increased range to ensure we don't block valid memory regions
     static bool IsValidPointer(uintptr_t address) {
-        return address > 0x100000000 && address < 0x3000000000;
-    }
-
-    static uintptr_t ReadMem(uintptr_t address) {
-        if (!IsValidPointer(address)) {
-            return 0;
-        }
-        return *reinterpret_cast<uintptr_t*>(address);
-    }
-
-    static void ReadMem(uintptr_t address, void* buffer, size_t size) {
-        if (!IsValidPointer(address)) {
-            return;
-        }
-        memcpy(buffer, reinterpret_cast<void*>(address), size);
-    }
-
-    template <typename T>
-    static void WriteMem(uintptr_t address, const T& value) {
-        if (!IsValidPointer(address)) {
-            return;
-        }
-        *reinterpret_cast<T*>(address) = value;
+        return address > 0x1000 && address < 0xFFFFFFFFFFFFFFFF;
     }
 
     static void WriteMem(uintptr_t address, const void* data, size_t size) {
-        if (!IsValidPointer(address)) {
-            return;
-        }
-        vm_protect(mach_task_self(), (vm_address_t)address, size, false, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+        if (!IsValidPointer(address)) return;
+
+        // 1. Get the current page size to align memory correctly
+        size_t pageSize = 0x4000; // iOS ARM64 page size
+        uintptr_t pageStart = address & ~(pageSize - 1);
+        size_t pageOffset = address - pageStart;
+        size_t protSize = (pageOffset + size + pageSize - 1) & ~(pageSize - 1);
+
+        // 2. Change protection to READ/WRITE/EXECUTE to allow patching
+        vm_protect(mach_task_self(), (vm_address_t)pageStart, protSize, false, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+        
+        // 3. Copy the bytes
         memcpy(reinterpret_cast<void*>(address), data, size);
-        vm_protect(mach_task_self(), (vm_address_t)address, size, false, VM_PROT_READ | VM_PROT_EXECUTE);
+        
+        // 4. Flush the Instruction Cache so the CPU sees the new code immediately
+        // This is the step most mod menus miss!
+        sys_icache_invalidate((void*)address, size);
+        
+        // 5. Restore protection to READ/EXECUTE
+        vm_protect(mach_task_self(), (vm_address_t)pageStart, protSize, false, VM_PROT_READ | VM_PROT_EXECUTE);
     }
 };
